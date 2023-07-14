807bc0c5477088786b5e26ae5430401d356d33e1
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-07-14 19:11:08 -0300

    making use of the fixture setup_teardown function and using the instance of browser defined at yield

--- tests/test_valid_login.py
+++ tests/test_valid_login.py
@@ -1,21 +1,24 @@
-from selenium import webdriver
+import time
+
+import pytest
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
 
-browser.get('https://www.saucedemo.com')
-browser.maximize_window()
-browser.implicitly_wait(10)
+@pytest.mark.usefixtures("setup_teardown")
+class TestCT01:
+    def test_ct01_test_valid_login(self, setup_teardown):
+        browser = setup_teardown
+        # find elements
+        user_name = browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]')
+        user_pass = browser.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 
-# find elements
-user_name = browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]')
-user_pass = browser.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+        user_name.send_keys('standard_user')
+        user_pass.send_keys('secret_sauce')
 
-user_name.send_keys('standard_user')
-user_pass.send_keys('secret_sauce')
+        time.sleep(2)
 
-browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
+        browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
 
-assert browser.find_element(By.CSS_SELECTOR, '.header_secondary_container > span.title').is_displayed()
+        assert browser.find_element(By.CSS_SELECTOR, '.header_secondary_container > span.title').is_displayed()
 
-browser.quit()
+        time.sleep(2)
