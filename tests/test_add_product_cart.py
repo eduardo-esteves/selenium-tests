4d28167feeec97a8cb46f226a2e4a63ddac0ba95
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-07-14 21:03:34 -0300

    making reuse of the setup_teardown function to initializing chrome webdriver by decorator usefixtures()

--- tests/test_add_product_cart.py
+++ tests/test_add_product_cart.py
@@ -1,38 +1,44 @@
 import time
 
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
-browser.maximize_window()
-browser.implicitly_wait(10)
 
-browser.get('https://www.saucedemo.com')
+@pytest.mark.usefixtures('setup_teardown')
+class TestCT02:
+    def test_ct02_add_product_cart(self, setup_teardown):
+        browser = setup_teardown
+        # find elements
+        user_name = browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]')
+        user_pass = browser.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 
-# find elements
-user_name = browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]')
-user_pass = browser.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+        user_name.send_keys('standard_user')
+        user_pass.send_keys('secret_sauce')
 
-user_name.send_keys('standard_user')
-user_pass.send_keys('secret_sauce')
+        time.sleep(2)
 
-# Clico no botão para logar
-browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
+        # Clico no botão para logar
+        browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
 
-assert browser.find_element(By.CSS_SELECTOR, '.header_secondary_container > span.title').is_displayed()
+        assert browser.find_element(By.CSS_SELECTOR, '.header_secondary_container > span.title').is_displayed()
 
-# Detalhes do pedido
-product = browser.find_element(By.XPATH, '//div[@class="inventory_item_name" and text()="Sauce Labs Backpack"]')
-product.click()
+        time.sleep(2)
 
-# Adicionar o produto ao carrinho
-browser.find_element(By.XPATH, '//button[contains(@class, "btn_inventory") and text()="Add to cart"]').click()
+        # Detalhes do pedido
+        product = browser.find_element(By.XPATH, '//div[@class="inventory_item_name" and text()="Sauce Labs Backpack"]')
+        product.click()
 
-# E clico no link do carrinho
-browser.find_element(By.ID, "shopping_cart_container").click()
+        time.sleep(2)
 
-time.sleep(5)
+        # Adicionar o produto ao carrinho
+        browser.find_element(By.XPATH, '//button[contains(@class, "btn_inventory") and text()="Add to cart"]').click()
 
-assert browser.find_element(By.XPATH, '//div[@class="inventory_item_name" and text()="Sauce Labs Backpack"]').is_displayed()
+        time.sleep(2)
 
-browser.quit()
+        # E clico no link do carrinho
+        browser.find_element(By.ID, "shopping_cart_container").click()
+
+        time.sleep(2)
+
+        assert browser.find_element(By.XPATH, '//div[@class="inventory_item_name" and text()="Sauce Labs Backpack"]').is_displayed()
+
