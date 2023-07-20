e1783ef88b6aa2a55cf71f7f72f8b4d39b946b7f
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-07-19 21:52:17 -0300

    refactored class TestCT01 to reuse the method make_login developed some time ago

--- tests/test_valid_login.py
+++ tests/test_valid_login.py
@@ -1,21 +1,14 @@
 import time
 
 from selenium.webdriver.common.by import By
-
+from pages.login_page import LoginPage
 
 class TestCT01:
     def test_ct01_test_valid_login(self, setup_teardown):
         browser = setup_teardown
-        # find elements
-        user_name = browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]')
-        user_pass = browser.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 
-        user_name.send_keys('standard_user')
-        user_pass.send_keys('secret_sauce')
-
-        time.sleep(2)
-
-        browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
+        login_page = LoginPage()
+        login_page.make_login('standard_user', 'secret_sauce', browser)
 
         assert browser.find_element(By.CSS_SELECTOR, '.header_secondary_container > span.title').is_displayed()
 
