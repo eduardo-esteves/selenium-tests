fb548fe6d126533e7928f52d53d9af083a7df99d
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-07-20 21:36:22 -0300

    refactored LoginPage class, improved and centralized all locator elements

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,11 +5,16 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
 
+    def __init__(self):
+        self.user_name = (By.CSS_SELECTOR, 'input[name="user-name"]')
+        self.user_passwd = (By.CSS_SELECTOR, 'input[name="password"]')
+        self.login_button = (By.CSS_SELECTOR, 'input[name="login-button"]')
+
     def make_login(self, user_name, user_pass, browser):
         # find elements
-        browser.find_element(By.CSS_SELECTOR, 'input[name="user-name"]').send_keys(user_name)
-        browser.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(user_pass)
+        browser.find_element(*self.user_name).send_keys(user_name)
+        browser.find_element(*self.user_passwd).send_keys(user_pass)
 
         time.sleep(2)
 
-        browser.find_element(By.CSS_SELECTOR, 'input[name="login-button"]').click()
+        browser.find_element(*self.login_button).click()
